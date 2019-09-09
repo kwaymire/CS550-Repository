c6e73e176b60613105b2fe7eda3586eb33103f1c
kwaymire <54952516+kwaymire@users.noreply.github.com>
2019-09-09 09:24:27 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,29 @@
+#Name: Kenadi Waymire
+#Date: 9/5/19
+#OMH
+#My code runs a simple "conversation" with some bot.
+#Sources: None
+
+
+print('')
+print('Hello, user!', end='')
+#This section of code is the introduction and learning of names
+userresponse = input(' How are you?\n')
+print('I understand. Me too!\n')
+username = input(' What is your name?\n')
+print(' Nice! My name is Python, but you can call me Thon.\n')
+classyear = input(' What is your class year?\n')
+print(' Cool! I am made by Kenadi, a fourth form student.\n')
+#This section of code is academic information
+favclass = input(' What is your favorite class?\n')
+print('Interesting. If I was a student at Choate, my favorite would be CS550HO.\n')
+#The rest of the code is personal--height, hair color, favorite color.
+location = input('Where are you from?\n')
+print('Nice, I am from Lanphier Center!\n')
+color = input('What is your favorite color?\n')
+print('Awesome, mine is $*@(!)%.\n')
+haircolor = input('What is the color of your hair?\n')
+print('That is so cool! My hair color is grey.\n')
+height = input('How tall are you?\n')
+print('I am 12 foot 6.\n')
+print('Well, I must be going. Have a great day!\n')
