4a93c770dc8630a2e30199211eeb1ecf20b58911
kwaymire <54952516+kwaymire@users.noreply.github.com>
2019-09-09 22:21:04 -0400

    Add files via upload

---
+++ weirddistance.py
@@ -0,0 +1,15 @@
+import math as m
+
+
+x1 = 0
+y1 = 0
+
+x2 = int(input('Please insert a value for the x-coordinate.\n'))
+
+
+
+y2 = int(input('Please insert a value for the y-coordinate.\n'))
+
+answer = int(m.sqrt(((x2 - x1)**2)+((y2-y1))))
+
+print (answer)
